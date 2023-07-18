4232397400174daf0d28197ba4606f260e038e3f
Geetha Krishna <geetha.c@beehyv.com>
2023-07-18 15:53:51 +0530

    modified product variant search logic

--- health-services/product/src/main/java/org/egov/product/service/ProductVariantService.java
+++ health-services/product/src/main/java/org/egov/product/service/ProductVariantService.java
@@ -1,13 +1,20 @@
 package org.egov.product.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.egov.common.data.query.builder.SelectQueryBuilder;
+import org.egov.common.data.query.exception.QueryBuilderException;
 import org.egov.common.models.product.ProductVariant;
 import org.egov.common.models.product.ProductVariantRequest;
 import org.egov.common.service.IdGenService;
 import org.egov.product.config.ProductConfiguration;
 import org.egov.product.repository.ProductVariantRepository;
+import org.egov.product.repository.rowmapper.ProductRowMapper;
+import org.egov.product.repository.rowmapper.ProductVariantRowMapper;
+import org.egov.product.web.models.ProductVariantSearch;
 import org.egov.product.web.models.ProductVariantSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,19 +22,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.egov.common.utils.CommonUtils.checkRowVersion;
-import static org.egov.common.utils.CommonUtils.enrichForCreate;
-import static org.egov.common.utils.CommonUtils.enrichForUpdate;
-import static org.egov.common.utils.CommonUtils.getIdToObjMap;
-import static org.egov.common.utils.CommonUtils.getSet;
-import static org.egov.common.utils.CommonUtils.getTenantId;
-import static org.egov.common.utils.CommonUtils.havingTenantId;
-import static org.egov.common.utils.CommonUtils.identifyNullIds;
-import static org.egov.common.utils.CommonUtils.includeDeleted;
-import static org.egov.common.utils.CommonUtils.isSearchByIdOnly;
-import static org.egov.common.utils.CommonUtils.lastChangedSince;
-import static org.egov.common.utils.CommonUtils.validateEntities;
-import static org.egov.common.utils.CommonUtils.validateIds;
+import static org.egov.common.utils.CommonUtils.*;
 
 @Service
 @Slf4j
@@ -40,15 +35,27 @@ public class ProductVariantService {
     private final ProductVariantRepository productVariantRepository;
 
     private final ProductConfiguration productConfiguration;
+    protected final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    protected final SelectQueryBuilder selectQueryBuilder;
+    protected final RowMapper rowMapper;
+
 
     @Autowired
     public ProductVariantService(IdGenService idGenService, ProductService productService,
                                  ProductVariantRepository productVariantRepository,
-                                 ProductConfiguration productConfiguration) {
+                                 ProductConfiguration productConfiguration,
+                                 NamedParameterJdbcTemplate namedParameterJdbcTemplate,
+                                 SelectQueryBuilder selectQueryBuilder,
+                                 ProductVariantRowMapper rowMapper) {
         this.idGenService = idGenService;
         this.productService = productService;
         this.productVariantRepository = productVariantRepository;
         this.productConfiguration = productConfiguration;
+        this.selectQueryBuilder = selectQueryBuilder;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.rowMapper = rowMapper;
+
+
     }
 
     public List<ProductVariant> create(ProductVariantRequest request) throws Exception {
@@ -114,7 +121,40 @@ public class ProductVariantService {
                     .collect(Collectors.toList());
         }
         log.info("searching product variants using criteria");
-        return productVariantRepository.find(productVariantSearchRequest.getProductVariant(),
+//        return productVariantRepository.find(productVariantSearchRequest.getProductVariant(),
+//                limit, offset, tenantId, lastChangedSince, includeDeleted);
+        return find(productVariantSearchRequest.getProductVariant(),
                 limit, offset, tenantId, lastChangedSince, includeDeleted);
+    }
+
+    public List<ProductVariant> find(ProductVariantSearch searchObject, Integer limit, Integer offset, String tenantId, Long lastChangedSince, Boolean includeDeleted) throws QueryBuilderException {
+        String tableName = "product_variant";
+        String query = this.selectQueryBuilder.build(searchObject);
+        Map<String, Object> paramsMap = this.selectQueryBuilder.getParamsMap();
+        query = query + " AND tenantId=:tenantId ";
+        if (query.contains(tableName + " AND")) {
+            query = query.replace(tableName + " AND", tableName + " WHERE");
+        }
+
+        if (query.contains("variation=:variation")) {
+            query = query.replace("variation=:variation", "variation ilike :variation");
+            paramsMap.put("variation", "%" + searchObject.getVariation() + "%");
+        }
+
+        if (Boolean.FALSE.equals(includeDeleted)) {
+            query = query + "AND isDeleted=:isDeleted ";
+        }
+
+        if (lastChangedSince != null) {
+            query = query + "AND lastModifiedTime>=:lastModifiedTime ";
+        }
+
+        query = query + "ORDER BY id ASC LIMIT :limit OFFSET :offset";
+        paramsMap.put("tenantId", tenantId);
+        paramsMap.put("isDeleted", includeDeleted);
+        paramsMap.put("lastModifiedTime", lastChangedSince);
+        paramsMap.put("limit", limit);
+        paramsMap.put("offset", offset);
+        return this.namedParameterJdbcTemplate.query(query, paramsMap, this.rowMapper);
     }
 }
