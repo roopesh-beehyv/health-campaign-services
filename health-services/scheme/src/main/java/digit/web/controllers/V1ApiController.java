2aebad62d7f333245dcce73d8afaf8655ddef90f
Geetha Krishna <geetha.c@beehyv.com>
2023-07-20 12:25:47 +0530

    bug fix: reformatted the code - an api was declared inside a function

--- health-services/scheme/src/main/java/digit/web/controllers/V1ApiController.java
+++ health-services/scheme/src/main/java/digit/web/controllers/V1ApiController.java
@@ -8,19 +8,18 @@ import digit.web.models.*;
 import io.swagger.annotations.ApiParam;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.egov.common.contract.request.RequestInfo;
 import org.egov.common.contract.response.ResponseInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -29,7 +28,7 @@ import java.util.List;
 @Controller
 @ToString
 @Slf4j
-@RequestMapping("/scheme-services")
+@RequestMapping("")
 public class V1ApiController {
     private final ObjectMapper objectMapper;
     private final HttpServletRequest request;
@@ -46,7 +45,6 @@ public class V1ApiController {
 
     @RequestMapping(value = "/v1/registration/_create", method = RequestMethod.POST)
     public ResponseEntity<SchemeRegistrationResponse> v1RegistrationCreatePost(@ApiParam(value = "Details for the new Application(s) + RequestInfo meta data.", required = true) @Valid @RequestBody SchemeRequest schemeRequest) {
-        log.info("coming here ---------------");
         List<SchemeApplication> applications = schemeRegistrationService.registerSchemeRequest(schemeRequest);
         ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(schemeRequest.getRequestInfo(), true);
         SchemeRegistrationResponse response = SchemeRegistrationResponse.builder().schemeApplications(applications).responseInfo(responseInfo).build();
@@ -56,18 +54,18 @@ public class V1ApiController {
     @RequestMapping(value = "/v1/registration/_update", method = RequestMethod.POST)
     public ResponseEntity<SchemeRegistrationResponse> v1RegistrationUpdatePost(@ApiParam(value = "Details for the new (s) + RequestInfo meta data.", required = true) @Valid @RequestBody SchemeRequest schemeRequest) {
         SchemeApplication application = schemeRegistrationService.updateSchApplication(schemeRequest);
-
         ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(schemeRequest.getRequestInfo(), true);
         SchemeRegistrationResponse response = SchemeRegistrationResponse.builder().schemeApplications(Collections.singletonList(application)).responseInfo(responseInfo).build();
         return new ResponseEntity<>(response, HttpStatus.OK);
 
-        @RequestMapping(value = "/v1/scheme/_search", method = RequestMethod.POST)
-        public ResponseEntity<SchemeRegistrationResponse> v1SchemeSearchPost (@RequestBody RequestInfoWrapper
-        requestInfoWrapper, @Valid @ModelAttribute SchemeApplicationSearchCriteria schemeApplicationSearchCriteria){
-            List<SchemeApplication> applications = schemeRegistrationService.searchSchemeApplications(requestInfoWrapper.getRequestInfo(), schemeApplicationSearchCriteria);
-            ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(requestInfoWrapper.getRequestInfo(), true);
-            SchemeRegistrationResponse response = SchemeRegistrationResponse.builder().schemeApplications(applications).responseInfo(responseInfo).build();
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
-
     }
+
+    @RequestMapping(value = "/v1/registration/_search", method = RequestMethod.POST)
+    public ResponseEntity<SchemeRegistrationResponse> v1SchemeSearchPost(@RequestBody RequestInfoWrapper requestInfoWrapper, @Valid @ModelAttribute SchemeApplicationSearchCriteria schemeApplicationSearchCriteria) {
+        List<SchemeApplication> applications = schemeRegistrationService.searchSchemeApplications(requestInfoWrapper.getRequestInfo(), schemeApplicationSearchCriteria);
+        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(requestInfoWrapper.getRequestInfo(), true);
+        SchemeRegistrationResponse response = SchemeRegistrationResponse.builder().schemeApplications(applications).responseInfo(responseInfo).build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
